a35dacabb38c19ad9f1b9681372d41e2e37759a4
Percell Gaskins <pmg8899@gmail.com>
2022-03-10 20:01:14 -0800

    "time"

---
+++ pycoin.py
@@ -0,0 +1,115 @@
+import json
+from pip._vendor import requests
+from tkinter import *
+
+
+
+pycoin = Tk()
+pycoin.title("PyCOIN Portfolio")
+
+
+
+def my_pycoin():
+    COIN_API = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD&CMC_PRO_API_KEY=510757cf-cf43-4f5e-8644-3c0b2e8f5589"
+
+    get_api = requests.get(COIN_API)
+
+    conv_api = json.loads(get_api.content)
+
+    coins  = [
+        {
+            "symbol" : "BTC",
+            "amount_owned": 55,
+            "price_per_coin" : 31913
+        },
+        {
+            "symbol" : "ETH",
+            "amount_owned" : 5,
+            "price_per_coin":2039
+        },
+        {
+            "symbol" : "USDT",
+            "amount_owned" : 4000,
+            "price_per_coin" : 1
+        },
+        {
+            "symbol" : "BNB",
+            "amount_owned" : 200,
+            "price_per_coin" : 360
+        },
+        {
+            "symbol" : "USDC",
+            "amount_owned" : 700,
+            "price_per_coin" : .1
+        },
+        {
+            "symbol" : "LUNA",
+            "amount_owned" : 200,
+            "price_per_coin" : 90
+        },
+        {
+            "symbol" : "DOGE",
+            "amount_owned" : 40000,
+            "price_per_coin" : .19
+        }
+    ]
+
+
+#for loop to loop through json DOM range 0,5
+#for loop through the coin in coins that have invested {coins}
+#if the coin in the api = the {coins} print sympbol,name,price 
+#without the if condition it would print 5 sets of data 
+    totalval = 0
+    coin_row = 1
+
+    for i in range(0, 12):
+            for coin in coins:
+                if conv_api["data"][i]["symbol"] == coin["symbol"]:
+                    total_payout = coin["amount_owned"] * coin["price_per_coin"]
+                    current_value = coin["amount_owned"] * conv_api["data"][i]["quote"]["USD"]["price"]
+                    pl_percoin = conv_api["data"][i]["quote"]["USD"]["price"] - coin["price_per_coin"]
+                    total_pl = pl_percoin * current_value
+                    totalval = total_payout * current_value
+                    #label text printed out within the specific row and column
+                    name = Label(pycoin, text=conv_api["data"][i]["name"], bg="gray", fg="black")
+                    name.grid(row=coin_row, column=0, sticky=N+S+E+W)
+
+                    price = Label(pycoin, text="${0:.2f}".format(conv_api["data"][i]["quote"]["USD"]["price"]), bg="white", fg="black")
+                    price.grid(row=coin_row, column=1, sticky=N+S+E+W)
+
+                    num_coin = Label(pycoin, text=coin["amount_owned"], bg="gray", fg="black")
+                    num_coin.grid(row=coin_row, column=2, sticky=N+S+E+W)
+
+                    total_payout = Label(pycoin, text="${0:.2f}".format(total_payout), bg="white", fg="black")
+                    total_payout.grid(row=coin_row, column=3, sticky=N+S+E+W)
+
+                    current_val = Label(pycoin, text="${0:.2F}".format(current_value), bg="gray", fg="black")
+                    current_val.grid(row=coin_row, column=4, sticky=N+S+E+W)
+
+                    coin_row = coin_row + 1
+
+            current_val = Label(pycoin, text="${0:.2F}".format(totalval), bg="gray", fg="black")
+            current_val.grid(row=coin_row, column=4, sticky=N+S+E+W)
+
+
+
+name = Label(pycoin, text="Coin Name", bg="blue", fg="white", font= "Lato 12 bold", padx="5", pady="5", borderwidth=3, relief="groove")
+name.grid(row=0, column=0, sticky=N+S+E+W)
+
+price = Label(pycoin, text="Price", bg="blue", fg="white",font= "Lato 12 bold", padx="5", pady="5", borderwidth=3, relief="groove")
+price.grid(row=0, column=1, sticky=N+S+E+W)
+
+num_coin = Label(pycoin, text="Coin Owned", bg="blue", fg="white",font= "Lato 12 bold", padx="5", pady="5", borderwidth=3, relief="groove")
+num_coin.grid(row=0, column=2, sticky=N+S+E+W)
+
+total_payout = Label(pycoin, text="Total Payout", bg="blue", fg="white",font= "Lato 12 bold", padx="5", pady="5", borderwidth=3, relief="groove")
+total_payout.grid(row=0, column=3, sticky=N+S+E+W)
+
+current_val = Label(pycoin, text="Current Value", bg="blue", fg="white",font= "Lato 12 bold", padx="5", pady="5", borderwidth=3, relief="groove")
+current_val.grid(row=0, column=4, sticky=N+S+E+W)
+
+my_pycoin()
+
+pycoin.mainloop()
+
+
